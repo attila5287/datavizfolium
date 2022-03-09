7f1498000f5d9b0e9b905615b3df69d1ca4b30d4
attila5287 <atiturkoz@hotmail.com>
2022-03-09 04:43:15 -0700

    geojson test

--- app.py
+++ app.py
@@ -1,32 +1,56 @@
 """ flask_example.py
 
-    Required packages:
-    - flask
-    - folium
+  Required packages:
+  - flask
+  - folium
 
-    Usage:
+  Usage:
 
-    Start the flask server by running:
+  Start the flask server by running:
 
-        $ python flask_example.py
+    $ python flask_example.py
 
-    And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
+  And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
 
 """
 
 from flask import Flask
-
+import requests
 import folium
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
+  m = folium.Map(
+    location=[36, -99], 
+    tiles="stamentoner", 
+    zoom_start=4,
+    control_scale=True,
+  )
+  
+  url = (
+    "https://raw.githubusercontent.com/python-visualization/folium/main/examples/data"
+    )
+  us_states = f"{url}/us-states.json"
+  geo_json_data = json.loads(requests.get(us_states).text)    
+    
+  folium.GeoJson(geo_json_data).add_to(m)
+  
+  title_html = '''
+             <h5 align="center" style="font-size:20px">
+             <a href="https://github.com/attila5287/datavizfolium/">
+             time slider coming soon
+             </a>
+             </h5>
+             '''
+  m.get_root().html.add_child(folium.Element(title_html))
+
+  
+  return m._repr_html_()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
