a92d53eb60002e88070a07ddec2ab4c08d45fea3
attila5287 <atiturkoz@hotmail.com>
2022-03-11 04:35:12 -0700

    time slider

--- app.py
+++ app.py
@@ -15,30 +15,107 @@
 """
 
 from flask import Flask
+import csv
+import urllib.request
+import json
 import requests
 import folium
-import json
-
+from folium.plugins import TimeSliderChoropleth
+from branca.colormap import linear
+from datetime import datetime 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+  pass
+  urljson = 'https://raw.githubusercontent.com/python-visualization/'\
+  +'folium/main/examples/data/us-states.json'  
+
+  urlcsv = 'https://gist.githubusercontent.com/attila5287/ab110dd7ca5dd9d3cdad110af4ef322d'+ \
+  '/raw/ce1575f6289219b31f4e86595f2f882d37d63758/stateue.csv'
+  response = urllib.request.urlopen(urlcsv)
+  lines = [l.decode('utf-8') for l in response.readlines()]
+  cr = csv.reader(lines)
+  raw = {}
+  for row in cr:
+      pass
+      raw[row[0]] = row[1:]
+  raw.pop('State')
+
+  listed = {}
+  for state, lst in raw.items():
+      pass
+      if state == 'D.C.':
+          pass
+          state_name = 'DistrictofColumbia'
+      else:
+          pass
+          state_name = state.replace(' ', '')
+      listed[state_name] = [
+          float(n) for n in lst
+          ]
+
+  mins= [
+      min(v) for k,v in listed.items()
+  ]
+  maxs= [
+      max(v) for k,v in listed.items()
+  ]
+  minrate = min(mins)    
+  maxrate = max(maxs)
+
+  from branca.colormap import linear
+  cmap = linear.RdPu_09.scale( minrate, maxrate)
+  # cmap
+
+  colors = listed
+  for state, rates in listed.items():
+      pass
+      colors[state] = [
+          cmap(rate) for rate in rates
+      ]
+
+
+  dates = [str(round(datetime(2021, i+1, 1, 0).timestamp())) for i in range(len(colors['Colorado']))]
+
+  style_dict = {}
+  for state, cols in colors.items():
+      pass
+      d={}
+      for date, col in zip(dates, cols):
+          pass
+          d[date] = {
+              'color' : col,
+              'opacity' : 0.66,
+          }
+          style_dict[state] = d
+
+  geo = json.loads(requests.get(urljson).text)
+  for g in geo[ 'features' ]:
+    pass
+    g['id'] = g['properties']['name'].replace(' ', '')
+    g['properties']['name'] = g['properties']['name'].replace(' ', '')
+      
+  # geo['features'][0]
+
   m = folium.Map(
-    location=[36, -99], 
-    tiles="stamentoner", 
-    zoom_start=4,
-    control_scale=True,
-  )
-  
-  url = (
-    "https://raw.githubusercontent.com/python-visualization/folium/main/examples/data"
-    )
-  us_states = f"{url}/us-states.json"
-  geo_json_data = json.loads(requests.get(us_states).text)    
-    
-  folium.GeoJson(geo_json_data).add_to(m)
-  
+      [36, -99], 
+      tiles="stamentoner", 
+      zoom_start=4,
+      control_scale=True,
+      )
+
+  g = TimeSliderChoropleth(
+      geo,
+      styledict=style_dict,
+      ).add_to(m)
+  #specify the min and max values of your data
+  colormap = cmap
+  colormap.caption = 'State Unemployment Rate'
+  colormap.add_to(m)
+  m
+
   title_html = '''
              <h5 align="center" style="font-size:20px">
              <a href="https://github.com/attila5287/datavizfolium/">
